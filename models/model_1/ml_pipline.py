5f0b752055f6b0deeb72d7674c016e4340a0a251
xefr762 <idkryuchkov01@gmail.com>
2025-02-21 11:56:23 +0300

    added pipline for ML task

---
+++ models/model_1/ml_pipline.py
@@ -0,0 +1,37 @@
+import re
+import nltk
+import string
+import pymorphy2
+from nltk.corpus import stopwords
+
+import pandas as pd
+import numpy as numpy
+import joblib
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+nltk.download('stopwords')
+
+morph = pymorphy2.MorphAnalyzer()
+stop_words = set(stopwords.words('russian'))
+
+def clean_text_for_person(text):
+    if not isinstance(text, str):  # Проверяем, что текст - строка
+        return [""]
+
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))  # Удаляем пунктуацию
+    tokens = text.split()  # Разбиваем на слова
+    tokens = [morph.parse(word)[0].normal_form for word in tokens if word not in stop_words]  # Лемматизация
+    tokens = [word for word in tokens if len(word) > 1]  # Убираем короткие слова
+    cleaned_text = " ".join(tokens)
+
+    return [cleaned_text]
+
+vectorizer = joblib.load('vectorizer.joblib')
+
+def pipline(text):
+    clean_text = clean_text_for_person(text)
+    vectorize = vectorizer(clean_text)
+    return(vectorize)
